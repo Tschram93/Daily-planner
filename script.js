9e5588b1e41d7f1666c279d046782cfe969ce818
TS93 <tschram93@yahoo.com>
2021-08-21 05:10:18 -0400

    added psuedo code

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // moment().format('dddd'); 
 // moment().format("MMM Do YY");
 
+$(function () {});
+
+// Add today's date
+
+// Add hours to the schedule
+
+// Add to local storage 
+
+// Grid and Rows
+
+// 
