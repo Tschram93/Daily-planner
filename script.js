156cab3527f15a11f8af4c3de119bd35df3bcf0f
TS93 <tschram93@yahoo.com>
2021-08-27 17:34:25 -0400

    Event listener added into input box(may or may not work)

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-// moment().format('dddd'); 
+moment().format('dddd');
 
-// var today   = moment().format("MMM Do YY");
-// var current = moment().format("")
+var today = moment().format("MMM Do YY");
+var current = moment().format("")
 
 $(function () {});
 
 // Add today's date
 
 // Add hours to the schedule
-var hours = [
-    {time: '9:00 AM', event: '' },
-    {time: '10:00 AM', event: '' },
-    {time: '11:00 AM', event: '' },
-    {time: '12:00 PM', event: '' },
-    {time: '1:00 PM', event: '' },
-    {time: '2:00 PM', event: '' },
-    {time: '3:00 PM', event: '' },
-    {time: '4:00 PM', event: '' },
-    {time: '5:00 PM', event: '' },
+var hours = [{
+        time: '9:00 AM',
+        event: ''
+    },
+    {
+        time: '10:00 AM',
+        event: ''
+    },
+    {
+        time: '11:00 AM',
+        event: ''
+    },
+    {
+        time: '12:00 PM',
+        event: ''
+    },
+    {
+        time: '1:00 PM',
+        event: ''
+    },
+    {
+        time: '2:00 PM',
+        event: ''
+    },
+    {
+        time: '3:00 PM',
+        event: ''
+    },
+    {
+        time: '4:00 PM',
+        event: ''
+    },
+    {
+        time: '5:00 PM',
+        event: ''
+    },
 ];
 
 // Add to local storage 
 var tasks = JSON.parse(localStorage.getItem('Day'))
 
 // Shows current date
-$('#currentDay').text(moment().format( "MMMM Do YYYY, h:mm:ss a"));
+$('#currentDay').text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 //  Adding moment.js
 let now = new Date().getHours();
 
@@ -37,8 +63,8 @@ for (i = 0; i < hours.length; i++) {
         .attr('placeholder', 'Enter task here')
         .appendTo('.row');
     $(`<button class='col-2 btn btn-secondary saveBtn' id='hour${i}'></button>`)
-    .text('save')
-    .appendTo('.row');
+        .text('save')
+        .appendTo('.row');
     console.log(hours[i]);
 }
 
@@ -46,10 +72,16 @@ for (i = 0; i < hours.length; i++) {
 row10amInput.value = localStorage.getItem('saved');
 
 // Add eventlistener and saves the input data
-//hour#.addEventListener('click', userDataStore#);
+querySelector("textarea").addEventListener('input', e => {
+    let inputData = document.querySelector("textarea").value;
+    console.log(inputData);
+})
+// prevents page from refreshing
+// .preventDefault();
+
+// hour#.addEventListener('click', userDataStore#);
 
 // Saves to local storage
 function savedInfo() {
     localStorage.setItem('saved0', row9amInput.value);
 };
-
